b0243707670523e4df9216f5764c77761b600ffc
smenaq <sergiosantosmenaq@gmail.com>
2023-04-02 14:49:02 -0500

    use of payload

--- app/src/components/Counter.tsx
+++ app/src/components/Counter.tsx
@@ -1,20 +1,25 @@
-import {useReducer} from 'react'
+import React, {useReducer} from 'react'
 
 type stateStructure = {
     count : number
 }
 
-type doAction = 'increment' | 'decrement'
+type types = 'increment' | 'decrement'  | 'reset'
 
 type actionStructure = {
-    type : doAction
+    type : types, // define the type of action
+    payload ? : number // some information can send 
 }
 
 // necessary declare reducer function outside
 const reducer = (state : stateStructure, action : actionStructure) : stateStructure => {
+
+    const payload = action.payload? action.payload : 1
+
     switch(action.type) {
-        case 'increment' : return {count : state.count + 1}
-        case 'decrement' : return {count : state.count - 1}
+        case 'increment' : return {count : state.count + payload}
+        case 'decrement' : return {count : state.count - payload}
+        case 'reset' : return initialState
         default : return state
     }
 }
@@ -25,15 +30,14 @@ const initialState : stateStructure = {count : 0}
 function Counter() {
 
     const [state, dispatch] = useReducer(reducer, initialState)
-
-    const add = () => dispatch({type: 'increment'})
-    const substract = () => dispatch({type: 'decrement'})
-
     return  (
         <div className="counter">
             <h2>Counter</h2>
-            <button className='+' onClick={add}>+</button>
-            <button className='-' onClick={substract}>-</button>
+            <button className='+' onClick={() => dispatch({type: 'increment'})}>+</button>
+            <button className='-' onClick={() => dispatch({type: 'decrement'})}>-</button>
+            <button className='+' onClick={() => dispatch({type:'increment', payload: 5})}>+5</button>
+            <button className='-' onClick={() => dispatch({type: 'decrement', payload: 5})}>-5</button>
+            <button className='reset' onClick={() => dispatch({type: 'reset'})}>0</button>
             <h3>{state.count}</h3>
         </div>
     )
